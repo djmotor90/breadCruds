710a39335a756a3df472deca671b431ce3543256
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-09-16 11:51:42 -0500

    BreadCRUD Validation (be-8-schema-validation)

--- models/bread.js
+++ models/bread.js
@@ -7,7 +7,8 @@ const breadSchema = new Schema(
   {
       name      : {type : String, required: true},
       hasGluten : {type : Boolean},
-      image     : {type : String, default: 'http://placehold.it/500x500.png'}  
+      image     : {type : String, default: 'http://placehold.it/500x500.png'},
+      baker: {type: String, enum:['Rachel','Monica','Joey','Chandler','Ross','Phoebe']}  
   }
 );
 //Purpose: Use said schema to creat a model 
--- views/showBreadInfo.jsx
+++ views/showBreadInfo.jsx
@@ -16,12 +16,14 @@ function Show ({bread, index}) {
             have gluten.
         </p>
         <img src={bread.image} alt={bread.name} />
+        <p>Baked by {bread.baker}</p>
+        <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+       
         <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
 </form>
-
-        <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
-        <li><a href="/breads">Go home</a></li>
+<li><a href="/breads">Go home</a></li>
+       
         </Default>
     )
 }
